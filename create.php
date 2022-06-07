7beabbc235d75fb76d5e8b3713ecb8d4081c350c
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-06-07 15:44:32 +0700

    menambahkan server-side validasi

--- create.php
+++ create.php
@@ -3,15 +3,22 @@ include 'functions.php';
 $pdo = pdo_connect();
 
 if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $title = $_POST['title'];
-    $created = date('Y-m-d H:i:s');
-    // Insert new record into the contacts table
-    $stmt = $pdo->prepare('INSERT INTO contacts VALUES (?, ?, ?, ?, ?, ?)');
-    $stmt->execute([$id, $name, $email, $phone, $title, $created]);
-    header("location:index.php");
+    if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        echo "<p><span style=\"color: red\">Sorry, the email address you provided is not valid.</span></p>";
+    } else {
+        if (!filter_var($_POST['phone'], FILTER_VALIDATE_INT)) {
+            echo "<p><span style=\"color: red\">Sorry, phone number you provided is not valid.</span></p>";
+        } else {
+            $email = $_POST["email"]
+            $name = $_POST['name'];
+            $phone = $_POST['phone'];
+            $title = $_POST['title'];
+            $created = date('Y-m-d H:i:s');
+            // Insert new record into the contacts table
+            $stmt = $pdo->prepare('INSERT INTO contacts VALUES (?, ?, ?, ?, ?, ?)');
+            $stmt->execute([$id, $name, $email, $phone, $title, $created]);
+            header("location:index.php");
+        }
 }
 ?>
 <!DOCTYPE html>
