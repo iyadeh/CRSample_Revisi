0e7e99732ef33892ea58c44f718f8383467fe27f
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-06-12 12:20:45 +0700

    Menambahkan CSRF Token di form

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
-<?php include "functions.php"; ?>
+<?php include "functions.php"; 
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,38 +12,25 @@
 </head>
 <body class="text-center">
     <?php
-    $notif = null;
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-
-        session_start();
-        $user = $_POST['username'];
-        $pass = $_POST['password'];
-        $pdo = pdo_connect();
-        //$stmt = $pdo->prepare('SELECT * FROM users WHERE username = "' . $user . '" AND password = "' . password_hash($pass, PASSWORD_BCRYPT) . '" LIMIT 1');
-        $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1');
-        $stmt->bindParam(1, $user);
-        $stmt->bindParam(2, password_hash($pass, PASSWORD_BCRYPT));
-        $stmt->execute();
-        $notif = $stmt->rowCount();
-        if ($stmt->rowCount() > 0) {
-            $_SESSION['user'] = $user;
-            header("location: index.php");
-        } else {
-            $notif = "Wrong usename or password";
-        }
+    if(!isset($_SESSION['notif'])){
+        $_SESSION['notif']="";
     }
+    $_SESSION['token'] = bin2hex(random_bytes(35));
 
     ?>
-    <form class="form-signin" method="POST">
+    <form action="post.php" class="form-signin" method="POST">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="inputUsername" class="sr-only">Username</label>
         <input type="username" id="inputUsername" name="username" class="form-control" placeholder="Username" required autofocus>
         <br>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+        <input type="hidden" name="token" value="<?= $_SESSION['token'] ?? '' ?>">
         <div class="checkbox mb-3">
             <label>
-                <?= $notif ?>
+                <?php 
+                    echo $_SESSION['notif'];
+                ?>
             </label>
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
